5b78b9e8ac298ed17270a0f44482386d263f05b6
RashadXalil <rashadmkh@code.edu.az>
2022-10-05 09:46:57 +0400

    inital

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import Footer from './components/footer/Footer'
+import Header from './components/header/Header'
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Footer />
+    </div>
+  )
+}
+
+export default App
