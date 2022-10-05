539d8aea28eaaf752c24b5ed34f7b78ba4cc19ec
RashadXalil <rashadmkh@code.edu.az>
2022-10-05 10:43:45 +0400

    add page ready

--- src/pages/users/add/Add.jsx
+++ src/pages/users/add/Add.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Add.css'
 import {
   FormControl,
@@ -8,27 +8,69 @@ import {
   Input,
   Button,
 } from '@chakra-ui/react'
+import axios from 'axios'
 const Add = () => {
+  let defaultDate = new Date()
+  defaultDate.setDate(defaultDate.getDate() + 4)
+  const [firstname, setFirstname] = useState('')
+  const [lastname, setLastname] = useState('')
+  const [email, setEmail] = useState('')
+  const [birthday, setBirthday] = useState(defaultDate)
+  const addHandler = () => {
+    axios
+      .post('http://localhost:8080/users', {
+        firstname: firstname,
+        lastname: lastname,
+        email: email,
+        birthday: birthday,
+      })
+      .then((res) => console.log(res))
+  }
   return (
     <div className="add__form">
       <p>Add User</p>
       <FormControl>
         <FormLabel>First Name</FormLabel>
-        <Input type="email" placeholder="firstname" />
+        <Input
+          type="email"
+          placeholder="firstname"
+          onChange={(e) => {
+            setFirstname(e.target.value)
+          }}
+        />
       </FormControl>
       <FormControl>
         <FormLabel>Last Name</FormLabel>
-        <Input type="email" placeholder="lastname" />
+        <Input
+          type="email"
+          placeholder="lastname"
+          onChange={(e) => {
+            setLastname(e.target.value)
+          }}
+        />
       </FormControl>
       <FormControl>
         <FormLabel>Email</FormLabel>
-        <Input type="email" placeholder="email" />
+        <Input
+          type="email"
+          placeholder="email"
+          onChange={(e) => {
+            setEmail(e.target.value)
+          }}
+        />
       </FormControl>
       <FormControl>
         <FormLabel>Birthday</FormLabel>
-        <Input type="date" />
+        <Input
+          type="date"
+          onChange={(e) => {
+            setBirthday(e.target.value)
+          }}
+        />
       </FormControl>
-      <Button className="add__btn">Add</Button>
+      <Button className="add__btn" onClick={addHandler}>
+        Add
+      </Button>
     </div>
   )
 }
