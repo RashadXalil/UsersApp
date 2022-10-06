cea18f306822add0d628361f7c5a7805f445f763
RashadXalil <rashadmkh@code.edu.az>
2022-10-06 10:09:16 +0400

    linters added to project

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
     <BrowserRouter>
       <Header />
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/users" element={<Users />} />
         <Route path="/add" element={<Add />} />
